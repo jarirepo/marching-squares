a2b377c16c29c7150e36d9495720f47d64c6ce0f
Jari Repo <jarirepo76@gmail.com>
2020-08-01 01:48:15 +0200

    Improved Marching Square update

--- src/marching-squares.ts
+++ src/marching-squares.ts
@@ -5,62 +5,42 @@ interface MarchingSquaresOptions {
 	isoValue: number;
 }
 
+/** Mapping from cell code 0-15 to contour edges indices */
+const cell2edge: number[][][] = [
+	[],
+	[[2, 3]],
+	[[1, 2]],
+	[[1, 3]],
+	[[0, 1]],
+	[[0, 1], [2, 3]],
+	[[0, 2]],
+	[[0, 3]],
+	[[0, 3]],
+	[[0, 2]],
+	[[0, 3], [1, 2]],
+	[[0, 1]],
+	[[1, 3]],
+	[[1, 2]],
+	[[2, 3]],
+	[]
+];
+
 export class MarchingSquares {
 
 	contours: Vertex[][] = [];
 
-	constructor(readonly grid: Grid, readonly opts: MarchingSquaresOptions) {
-	}
+	constructor(readonly grid: Grid, readonly opts: MarchingSquaresOptions) { }
 
 	update(): void {
-		// const weights = [8, 4, 2, 1];
-
 		// Evaluate the vertices
 		this.grid.vertices.forEach(v => v.evaluate(this.opts.isoValue));
-
 		// Interpolate the edges
 		this.grid.edges.forEach(e => e.interpolate(this.opts.isoValue));
-
 		// Generate contour lines
 		this.contours = [];
 		for (const cell of this.grid.cells) {
-			switch (cell.code) {
-				case 1:
-				case 14:					
-					this.contours.push([cell.edges[2].q, cell.edges[3].q])
-					break;
-				case 2:
-				case 13:
-					this.contours.push([cell.edges[1].q, cell.edges[2].q])
-					break;
-				case 3:
-				case 12:
-					this.contours.push([cell.edges[1].q, cell.edges[3].q])
-					break;
-				case 4:
-				case 11:
-					this.contours.push([cell.edges[0].q, cell.edges[1].q])
-					break;
-				case 5:
-					this.contours.push([cell.edges[0].q, cell.edges[1].q])
-					this.contours.push([cell.edges[2].q, cell.edges[3].q])
-					break;
-				case 10:
-					this.contours.push([cell.edges[0].q, cell.edges[3].q])
-					this.contours.push([cell.edges[1].q, cell.edges[2].q])
-					break;
-				case 6:
-				case 9:
-					this.contours.push([cell.edges[0].q, cell.edges[2].q])
-					break;
-				case 7:
-				case 8:
-					this.contours.push([cell.edges[0].q, cell.edges[3].q])
-					break;
-			}
+			this.contours.push(...cell2edge[cell.code].map(v => v.map(i => cell.edges[i].q)));
 		}
-
-		// console.log('Contours:', this.contours);
 	}
 
 	public show(context: CanvasRenderingContext2D): void {
